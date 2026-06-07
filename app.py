8c15c202eb2ebaff37e61e0273625ea08876c1cd
doug0816 <mkzv7ck29g@privaterelay.appleid.com>
2026-06-08 05:57:22 +0800

    add background image

--- app.py
+++ app.py
@@ -1,34 +1,38 @@
 from flask import Flask
+import os
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
     return """
-    <h1><h1>鄭道可個人網站</h1></h1>
+    <body style="
+        margin:0;
+        min-height:100vh;
+        background-image:url('/static/background.jpg');
+        background-size:cover;
+        background-position:center;
+        background-repeat:no-repeat;
+        color:white;
+        text-align:center;
+        padding-top:100px;
+        font-family:Arial;
+    ">
 
-    <img src="https://picsum.photos/600/300" width="600">
+    <h1>鄭道可個人網站</h1>
 
-    <br><br>
+    <a style="color:white;" href="/about">關於我</a><br><br>
+    <a style="color:white;" href="/works">我的作品</a><br><br>
+    <a style="color:white;" href="/contact">聯絡方式</a>
 
-    <a href="/about">關於我</a>
-
-    <br><br>
-
-    <a href="/works">我的作品</a>
-
-    <br><br>
-
-    <a href="/contact">聯絡方式</a>
+    </body>
     """
 
 @app.route("/about")
 def about():
     return """
     <h1>關於我</h1>
-
     <p>我是 Douglas。</p>
-
     <a href="/">回首頁</a>
     """
 
@@ -36,9 +40,7 @@ def about():
 def works():
     return """
     <h1>我的作品</h1>
-
     <p>這裡之後可以放 Blender 作品。</p>
-
     <a href="/">回首頁</a>
     """
 
@@ -46,17 +48,10 @@ def works():
 def contact():
     return """
     <h1>聯絡方式</h1>
-
     <p>Instagram：@DOARTS</p>
-
     <a href="/">回首頁</a>
     """
-
-import os
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port)
-
-    
-           
