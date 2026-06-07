e045acc28fd385d82a50fa722eab5b53b458add2
doug0816 <mkzv7ck29g@privaterelay.appleid.com>
2026-06-08 02:09:33 +0800

    render fix

--- app.py
+++ app.py
@@ -52,6 +52,11 @@ def contact():
     <a href="/">回首頁</a>
     """
 
+import os
+
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
+
+    
            
